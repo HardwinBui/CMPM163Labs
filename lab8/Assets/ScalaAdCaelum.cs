9179e9e4d2114abb97370fd0228723dbc3546928
Hardwin Bui <buihardwin@yahoo.com>
2020-05-25 09:16:28 -0700

    multiple towers are generated in the scene now

--- lab8/Assets/ScalaAdCaelum.cs
+++ lab8/Assets/ScalaAdCaelum.cs
@@ -6,36 +6,23 @@ public class ScalaAdCaelum : MonoBehaviour {
 
     public GameObject[] buildings;
     public GameObject[] towerTops;
-    public int mapWidth = 20;
-    public int mapHeight = 20;
+    public int mapWidth = 5;
+    public int mapHeight = 5;
 
-    public int minTowerDist = 100;
+    public int minTowerDist = 1;
     public int buildingFootprint = 3; // Assuming building are cubes and this defines length of side
     public int radius = 10; // Radius of bottom most layer
     public int angleMul = 10;
 
     // Start is called before the first frame update
     void Start() {
-
-        float seed = Random.Range(0, 100);
-
-/*
-        for(int h = 0; h < mapHeight; h++) {
+       // Place random towers in grid pattern
+       for(int h = 0; h < mapHeight; h++) {
             for(int w = 0; w < mapWidth; w++) {
-                int result = (int) (Mathf.PerlinNoise(w/10.0f + seed, h/10.0f + seed) * 10);
-                Vector3 pos = new Vector3(w * buildingFootprint, 0, h * buildingFootprint);
-                //int n = Random.Range(0, buildings.Length);
-                //Instantiate(buildings[n], pos, Quaternion.identity);
-                if(result < 2)       Instantiate(buildings[0], pos, Quaternion.identity);
-                else if(result < 4)  Instantiate(buildings[1], pos, Quaternion.identity);
-                else if(result < 6)  Instantiate(buildings[2], pos, Quaternion.identity);
-                else if(result < 7)  Instantiate(buildings[3], pos, Quaternion.identity);
-                else if(result < 8)  Instantiate(buildings[4], pos, Quaternion.identity);
-                else if(result < 10) Instantiate(buildings[5], pos, Quaternion.identity);
+                Vector3 pos = new Vector3(w * minTowerDist, 0, h * minTowerDist);
+                ProduceTower(pos);
             }
         }
-        */
-        ProduceTower(new Vector3(2, 0, 7));
     }
 
     private void ProduceTower(Vector3 origin) {
